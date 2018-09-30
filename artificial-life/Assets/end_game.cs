5af8de4e7bd9c74ed379b7607314fda38deb03e7
Josh Mendez <rafmendez92@yahoo.com>
2018-09-29 19:12:01 -0700

    Added Menu and game over screen

--- artificial-life/Assets/end_game.cs
+++ artificial-life/Assets/end_game.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class end_game : MonoBehaviour {
+public class end_game : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Game_Over_Screen");
+        }
+    }
 }
