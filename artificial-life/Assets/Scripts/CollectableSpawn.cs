ef70719705cd253b0c3020507e84d896455813d1
baterra <32109799+baterra@users.noreply.github.com>
2018-09-29 17:15:29 -0700

    Collectables "spawn" after you get one

---
+++ artificial-life/Assets/Scripts/CollectableSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawn : MonoBehaviour {
+    public GameObject[] Collectables;
+    
+    private int counter = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Collectables[counter] == null)
+        {
+            counter++;
+            if (counter > 3) { counter = 3; }
+        }
+        else {
+            Collectables[counter].GetComponent<SpriteRenderer>().enabled = true;
+            Collectables[counter].GetComponent<BoxCollider2D>().enabled = true;
+        }
+
+
+
+       // for (int i = 0; i < 4; i++)
+       // {
+         //   if (i == counter)
+          //  {
+                //Collectables[counter].GetComponent<SpriteRenderer>().enabled = true;
+           // }
+       // }
+        
+
+    }
+}
